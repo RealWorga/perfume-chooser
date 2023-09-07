03d827f34993b4c6aa69509c1beb706671f02cca
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-09-07 02:56:59 +0200

    Capitalize constants

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.perfume_chooser import rank_perfumes
 from src.pdf_generator import generate_pdf
-from config import output_file_path, num_top_perfumes
+from config import OUTPUT_FILE_PATH, NUM_TOP_PERFUMES
 
 def save_top_perfumes():
     ranked_df = rank_perfumes()
     if ranked_df is not None:
-        top_perfumes = ranked_df.head(num_top_perfumes)
-        top_perfumes.to_csv(output_file_path, index=False)
+        top_perfumes = ranked_df.head(NUM_TOP_PERFUMES)
+        top_perfumes.to_csv(OUTPUT_FILE_PATH, index=False)
         generate_pdf(top_perfumes)
 
 if __name__ == '__main__':
