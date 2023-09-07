cc0de51b9a6393d847d028e90ba9f02096bbe9ec
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-09-07 02:56:15 +0200

    Capitalize constants & Add category data configs

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
-# Configuration parameters
-project_root = os.path.dirname(os.path.realpath(__file__))
-input_file_path = os.path.join(project_root, 'data', 'perfume_data.csv')
-output_file_path = os.path.join(project_root, 'outputs', 'chosen_perfumes_budget.csv')
-pdf_output_path = os.path.join(project_root, 'outputs', 'chosen_perfumes_budget.pdf')
-num_top_perfumes = 21
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+INPUT_FILE_PATH = os.path.join(PROJECT_ROOT, 'data', 'perfume_data.csv')
+OUTPUT_FILE_PATH = os.path.join(PROJECT_ROOT, 'outputs', 'chosen_perfumes_budget.csv')
+PDF_OUTPUT_PATH = os.path.join(PROJECT_ROOT, 'outputs', 'chosen_perfumes_budget.pdf')
+
+NUM_TOP_PERFUMES = 21
+
+#Temporary location for the perfume datas from the different categories
+CATEGORICAL_DATA_PATH = os.path.join(PROJECT_ROOT, 'data', 'perfume_categorical_datas')
+
+CAT_OUTPUT_PATH = os.path.join(PROJECT_ROOT, "outputs", "perfume_cat_datas")
+CAT_OUTPUT_FIXED_PATH = os.path.join(PROJECT_ROOT, "outputs", "perfume_cat_datas_fixed")
