833e35db264a79487a07739fe3da1ed6692d4227
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-09-08 15:09:25 +0200

    Make efficient and readable

--- combine_scores.py
+++ combine_scores.py
@@ -2,18 +2,21 @@ import csv
 import os
 from config import CAT_OUTPUT_PATH
 
-# Define the weights for each file type
+Z_SCORES = "sorted_final_perfume_z_scores.csv"
+BAYESIAN_AVERAGE = "sorted_final_perfume_bayesian_average.csv"
+HOTNESS = "sorted_final_perfume_hotness.csv"
+
 WEIGHTS = {
-    "sorted_final_perfume_z_scores.csv": 0.2,
-    "sorted_final_perfume_bayesian_average.csv": 0.4,
-    "sorted_final_perfume_hotness.csv": 0.4
+    Z_SCORES: 0.2,
+    BAYESIAN_AVERAGE: 0.4,
+    HOTNESS: 0.4
 }
 
 def load_data_from_csv(filename):
     data = {}
     with open(os.path.join(CAT_OUTPUT_PATH, filename), 'r', encoding='utf-8-sig') as csvfile:
         reader = csv.reader(csvfile)
-        next(reader)  # skip header
+        next(reader)
         for row in reader:
             perfume_name = row[0]
             score = float(row[1])
@@ -29,7 +32,6 @@ def combine_scores(data_lists):
             else:
                 combined_scores[perfume] = [score]
 
-    # Calculate weighted average
     for perfume, scores in combined_scores.items():
         weighted_sum = sum(WEIGHTS[file] * score for file, score in zip(WEIGHTS, scores))
         combined_scores[perfume] = weighted_sum / sum(WEIGHTS.values())
@@ -44,15 +46,12 @@ def write_combined_data_to_csv(data, output_filename):
         for row in sorted_data:
             writer.writerow(row)
 
-# Load data from CSVs
-bayesian_data = load_data_from_csv("sorted_final_perfume_bayesian_average.csv")
-hotness_data = load_data_from_csv("sorted_final_perfume_hotness.csv")
-zscore_data = load_data_from_csv("sorted_final_perfume_z_scores.csv")
+bayesian_data = load_data_from_csv(BAYESIAN_AVERAGE)
+hotness_data = load_data_from_csv(HOTNESS)
+zscore_data = load_data_from_csv(Z_SCORES)
 
-# Combine data
 combined_data = combine_scores([zscore_data, bayesian_data, hotness_data])
 
-# Write combined data to CSV
 write_combined_data_to_csv(combined_data, "combined_scores.csv")
 
 print("Files have been combined!")
